3e07a84e8da601e00470c79e54a8d55e7869805b
Luccas69 <luccas.rossarolla@gmail.com>
2023-09-20 22:15:48 -0400

    criação do rodapé

--- src/components/Cadastro/index.jsx
+++ src/components/Cadastro/index.jsx
@@ -2,31 +2,36 @@ import styles from './styles.module.css';
 
 export default function Cadastro() {
     return (
-        <form className={styles.form}>
-            <div className={styles.imagem}>
-                <img src='logo.png' alt="Logo" />
-            </div>
-            <div className={styles.inputGroup}>
-                <label className={styles.label} htmlFor="nome">Nome:</label>
-                <input className={styles.input} type="text" id="nome" />
-            </div>
+        <>
+            <form className={styles.form}>
+                <div className={styles.imagem}>
+                    <img src='logo.png' alt="Logo" />
+                </div>
+                <div className={styles.inputGroup}>
+                    <label className={styles.label} htmlFor="nome">Nome:</label>
+                    <input className={styles.input} type="text" id="nome" />
+                </div>
 
-            <div className={styles.inputGroup}>
-                <label className={styles.label} htmlFor="email">Email:</label>
-                <input className={styles.input} type="email" id="email" />
-            </div>
+                <div className={styles.inputGroup}>
+                    <label className={styles.label} htmlFor="email">Email:</label>
+                    <input className={styles.input} type="email" id="email" />
+                </div>
 
-            <div className={styles.inputGroup}>
-                <label className={styles.label} htmlFor="senha">Senha:</label>
-                <input className={styles.input} type="password" id="senha" />
-            </div>
+                <div className={styles.inputGroup}>
+                    <label className={styles.label} htmlFor="senha">Senha:</label>
+                    <input className={styles.input} type="password" id="senha" />
+                </div>
 
-            <div className={styles.inputGroup}>
-                <label className={styles.label} htmlFor="confirmarSenha">Confirmar Senha:</label>
-                <input className={styles.input} type="password" id="confirmarSenha" />
-            </div>
+                <div className={styles.inputGroup}>
+                    <label className={styles.label} htmlFor="confirmarSenha">Confirmar Senha:</label>
+                    <input className={styles.input} type="password" id="confirmarSenha" />
+                </div>
 
-            <button type="submit" className={styles.submitButton}>Criar conta</button>
-        </form>
+                <button type="submit" className={styles.submitButton}>Criar conta</button>
+            </form>
+            <div className={styles.imagemgrande}>
+                <img src='imagemcadastro.png' alt="Imagem de cadastro"/>
+            </div>
+        </>
     );
 }
