032a9435878038b9d9947e6ba96349b064f0a38c
Cristopher Aguiar <cristopheraguiar123@hotmail.com>
2023-09-25 22:16:47 -0400

    Pré-Finalização Página Inicial

--- src/components/Principal/index.jsx
+++ src/components/Principal/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './styles.module.css';
+import logo from '../../images/logo.svg';
 
 function Principal() {
   return (
     <main className={styles.main}>
-      <p style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 600 }}>Seja bem-vindo(a) a</p>
-      <p style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 500, color: '#225943' }}>Conectando conhecimento, transformando possibilidades.</p>
-      <p style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 400 }}>Nossa plataforma oferece aulas interativas e exercícios práticos para ajudá-lo a dominar conceitos e melhorar suas habilidades.</p>
+      
+      <p style={{ fontFamily: 'Poppins, sans-serif', fontSize: 40, fontWeight: 600}}>Seja bem-vindo(a) a 
+      <img style={{flexDirection: 'row'}} src={logo} alt="Logo da Plataforma" />
+      </p> 
+    <div className={styles.textocentral}>
+        <p style={{ fontFamily: 'Poppins, sans-serif', fontSize: 28, fontWeight: 500, color: '#225943' }}>Conectando conhecimento, transformando possibilidades.</p>
+    </div>
+      
+      <p style={{ fontFamily: 'Poppins, sans-serif', fontSize: 22, fontWeight: 400 }}>Nossa plataforma oferece aulas interativas e exercícios práticos para ajudá-lo a dominar conceitos e melhorar suas habilidades.</p>
+
     </main>
   );
 }
---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,26 @@
+// componente de rodapé
+
+import styles from './styles.module.css';
+
+export default function Rodape() {
+    return (
+        <footer>
+            <div className={styles.container}>
+                <div className={styles.logosprojeto}>
+                    <img src="logobranca.png" alt="Logo do Ifro" />
+                    <img src="logo_fslab.png" alt="Logo do FS Lab" />
+                </div>
+                <div className={styles.redessociais}>
+                    <img src="Instagram.png" alt="Logo do Instragram" />
+                    <img src="facebook.png" alt="Logo do Facebook" />
+                </div>
+            </div>
+            <div className={styles.descricao}>
+                <img src="Vector.svg" alt="imagem de logo" />
+                <p>Plataforma de Ensino - 2023. Todos os direitos reservados</p>
+            </div>
+        </footer>
+    )
+}
+
+
