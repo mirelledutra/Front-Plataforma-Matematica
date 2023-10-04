50675592c939995a544d7e59fc1dbb939f188e4f
Luccas69 <luccas.rossarolla@gmail.com>
2023-10-04 19:54:38 -0400

    criado funções de cadastrar

--- src/components/Cadastro/index.jsx
+++ src/components/Cadastro/index.jsx
@@ -3,36 +3,48 @@
 import styles from './styles.module.css';
 import Link from 'next/link';
 
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Cadastro() {
 
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [confirmarSenha, setConfirmarSenha] = useState('');
+    const router = useRouter();
+
+    const handleCadastro = async (e) => {
+        e.preventDefault();
+        router.push('/Login');
+    }
     return (
         <div className={styles.container}>
             <div className={styles.imagemgrande}>
                 <img src='imagemcadastro.png' alt="Imagem de cadastro" />
             </div>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleCadastro}>
                 <div className={styles.imagem}>
                     <img src='logo.png' alt="Logo" />
                 </div>
                 <div className={styles.inputGroup}>
                     <label className={styles.label} htmlFor="nome">Nome:</label>
-                    <input className={styles.input} type="text" id="nome" />
+                    <input className={styles.input} type="text" value={nome} id="nome" onChange={(e) => setNome(e.target.value)} />
                 </div>
 
                 <div className={styles.inputGroup}>
                     <label className={styles.label} htmlFor="email">Email:</label>
-                    <input className={styles.input} type="email" id="email" />
+                    <input className={styles.input} type="email" value={email} id="email" onChange={(e) => setEmail(e.target.value)} />
                 </div>
 
                 <div className={styles.inputGroup}>
                     <label className={styles.label} htmlFor="senha">Senha:</label>
-                    <input className={styles.input} type="password" id="senha" />
+                    <input className={styles.input} type="password" value={senha} id="senha" onChange={(e) => setSenha(e.target.value)} />
                 </div>
 
                 <div className={styles.inputGroup}>
                     <label className={styles.label} htmlFor="confirmarSenha">Confirmar Senha:</label>
-                    <input className={styles.input} type="password" id="confirmarSenha" />
+                    <input className={styles.input} type="password" value={confirmarSenha} id="confirmarSenha" onChange={(e) => setConfirmarSenha(e.target.value)} />
                 </div>
 
                 <button type="submit" className={styles.submitButton}>Criar conta</button>
