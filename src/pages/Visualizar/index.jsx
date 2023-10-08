ad7e0b2332d9d6a04a8c18af5796bfb1081ac17e
Cristopher Aguiar <cristopheraguiar123@hotmail.com>
2023-10-08 15:03:28 -0400

    Tabela Visualização do Usuário

---
+++ src/pages/Visualizar/index.jsx
@@ -0,0 +1,14 @@
+import Conteudo from "@/components/Conteudos-aula";
+import Rodape from "@/components/Rodape";
+import MenuLateral from "@/components/MenuLateral";
+
+
+export default function SaibaMais() {
+
+    return (
+        <>
+             <Conteudo />   
+             <Rodape />
+        </>
+    )
+}
